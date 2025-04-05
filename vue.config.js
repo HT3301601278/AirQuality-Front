ebe83a8f293b8aac8430a2f1889f576e8aedc54e
世间微斯人 <3301601278@qq.com>
2025-04-05 22:39:35 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // 开发服务器配置
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080', // 后端API地址
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '/api' // 不重写路径
-        }
-      }
-    }
+    client: {
+      overlay: false
+    },
+    port: 8081
   }
 })
